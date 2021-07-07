4ec9be906ee549721ef2226e4c8251234f590c58
Jarauta1 <diegojarauta8@gmail.com>
2021-07-07 20:38:35 +0200

    add delete user

--- users.js
+++ users.js
@@ -6,16 +6,31 @@ const fetch = require("node-fetch")
 
 router.post("/", function (req,res){
     let token = req.body.token
+    let type = req.body.type
     
     fetch("http://51.38.51.187:5050/api/v1/users",{
       method: "GET",
       headers: {
-        "Content-Type": "application/json",
-        "Accept": "*/*",
-        "Authorization": "Bearer"+token
-      },
-      body: JSON.stringify(),
-    }).then((res)=>{res.json()}).then((data)=>{ 
+        "Authorization": `${type} ${token}`
+      }
+    }).then((res)=>res.json()).then((data)=>{ 
+      res.send({data: data})
+    }).catch(err => {
+      res.send({err:err})
+    })
+})
+
+router.delete("/delete", function (req,res){
+    let id = req.body.id
+    let token = req.body.token
+    let type = req.body.type
+    
+    fetch(`http://51.38.51.187:5050/api/v1/users/${id}`,{
+      method: "DELETE",
+      headers: {
+        "Authorization": `${type} ${token}`
+      }
+    }).then((res)=>res.json()).then((data)=>{ 
       res.send({data: data})
     }).catch(err => {
       res.send({err:err})
