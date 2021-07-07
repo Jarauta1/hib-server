a4d515c2c52040084188ddcd2c55b039afdb65a0
jarauta1 <diegojarauta8@gmail.com>
2021-07-07 13:40:47 +0200

    solved fail signUp request body&header

--- logIn.js
+++ logIn.js
@@ -8,7 +8,6 @@ router.post("/", function (req,res){
 
     let passwordLogIn = req.body.passwordLogIn
     let emailLogIn = req.body.emailLogIn
-    console.log(passwordLogIn,emailLogIn)
     
     fetch("http://51.38.51.187:5050/api/v1/auth/log-in",{
           method: "POST",
@@ -21,17 +20,7 @@ router.post("/", function (req,res){
             password: passwordLogIn
         }),
         }).then((res)=>res.json()).then((data)=>{
-            res.send({data: data})
-          
-         /*  document.getElementById("mensajeRegistro").innerHTML = `<span>${res.mensaje}</span>`
-           setMensaje(res.mensaje)
-           console.log(res.mensaje)
-           if (res.registro == "si") {
-             setVuelta("volver")
-             console.log(res.usuario)
-             setUsuario(res.usuario)
-             localStorage.setItem("mail",res.usuario)
-           }  */    
+            res.send({data: data})    
         })
 })
 
--- signUp.js
+++ signUp.js
@@ -9,33 +9,29 @@ router.post("/", function (req,res){
     let surnameSignUp = req.body.surnameSignUp
     let passwordSignUp = req.body.passwordSignUp
     let emailSignUp = req.body.emailSignUp
+
+    let header
     
     fetch("http://51.38.51.187:5050/api/v1/auth/sign-up",{
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "Accept": "*/*"
-          },
-          body: JSON.stringify({
-            name: nameSignUp, 
-            surname: surnameSignUp, 
-            email: emailSignUp, 
-            password: passwordSignUp
-        }),
-        }).then((res)=>res.json()).then((data)=>{
-            /* res.send({data: data}) */
-            console.log(data)
-          
-         /*  document.getElementById("mensajeRegistro").innerHTML = `<span>${res.mensaje}</span>`
-           setMensaje(res.mensaje)
-           console.log(res.mensaje)
-           if (res.registro == "si") {
-             setVuelta("volver")
-             console.log(res.usuario)
-             setUsuario(res.usuario)
-             localStorage.setItem("mail",res.usuario)
-           }  */    
-        })
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "*/*"
+      },
+      body: JSON.stringify({
+        name: nameSignUp, 
+        surname: surnameSignUp, 
+        email: emailSignUp, 
+        password: passwordSignUp
+      }),
+    }).then((res)=>{
+      header = res.status
+      return(res.json())
+    }).then((data)=>{ 
+      res.send({data: data, header: header})
+    }).catch(err => {
+      res.send({header:header})
+    })
 })
 
  module.exports = router;
